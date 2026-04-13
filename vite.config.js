5711cbbdc92d73ee35cae68283212e2f0640e376
Harish Guru <harishguru334>
2026-04-13 21:32:35 +0530

    Frontend Project

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://hotelbackend-ih5k.onrender.com',
         changeOrigin: true,
       }
     }
